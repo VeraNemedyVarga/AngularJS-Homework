af9c50b5f4b119c3381d454d733a80240143e30e
Vera Némedy-Varga <veranemedyv@padthai.local>
2017-09-20 17:40:48 +0200

    have to figure out how to store data from requested json object in variable.Removing record by it's index worked well before

--- app.js
+++ app.js
@@ -1,39 +1,67 @@
-const app = angular.module('angularJS-Homework', []);
+
+let app = angular.module('App', []);
+
+
 
 app.controller('MainCtrl', [
 '$scope',
 'records',
-function($scope, records){
+'getJson',
+function($scope, records, getJson){
   $scope.test = 'Hello world!';
   $scope.records = records;
+  this.getJson = getJson;
+  let data;
+  function successHandler(res){
+    data = res
+  }
+  function failureHandler(res){
+    data = res;
+  }
+  console.log(data);
+
   $scope.addPerson = function(employee){
     records.addPerson($scope.name, $scope.job, $scope.age, $scope.nick, employee)
+    $scope.name = '';
   };
+
   $scope.removePerson = function(id){
     console.log(id);
-    records.persons.splice(id, 1);
-  }
-  $scope.prettify=function(object){
-    return angular.toJSON(records)
+    records.people.splice(id, 1);
   }
 }]);
 
-app.service('records', [function(){
+app.service('getJson', [
+  '$http',
+  function($http){
+    this.getData = function(successHandler, failureHandler) {
+      $http.get('/persons.json').then(function(response){
+        successHandler(response);    //data can't be used outside this function
+      }, function(response) {
+        failureHandler(response);
+      })
+
+    }
+
+}])
+
+
+app.service('records', [
+  '$http',
+  function($http){
     let main = {
-        persons :[
-            {"name": "kutya", "job": "woofer", "age": "3", "nick": "pupper", "employee": true},
-            {"name": "cica", "job": "destroyer", "age": "8", "nick": "kitter", "employee": false}
-        ],
-        addPerson : function(name, job, age, nick, employee) {
-          console.log("stepped into addPerson method");
-          main.persons.push({
-            name: name,
-            job: job,
-            age: age,
-            nick: nick,
-            employee: employee,
-          })
-        },
-    };
-    return main
+      addPerson : function(name, job, age, nick, employee) {
+        console.log("stepped into addPerson method");
+        console.log(main.persons);
+        main.persons.push({
+          name: name,
+          job: job,
+          age: age,
+          nick: nick,
+          employee: employee,
+        })
+
+    }
+  };
+  return main;
 }])
